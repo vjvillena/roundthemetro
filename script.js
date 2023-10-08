f7d59a623453427352b11ef9cef80422e9942803
vjvillena <53135499+vjvillena@users.noreply.github.com>
2023-10-08 17:42:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -27,7 +27,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextImage = document.getElementById("nextImage");
 
     // An array of unique image file names
-    const imageFiles = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg", "images/7.jpg",];
+    const imageFiles = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg",
+     "images/5.jpg", "images/6.jpg", "images/7.jpg", "images/8.jpg", "images/9.jpg",
+     "images/10.jpg", "images/11.jpg", "images/12.jpg", "images/13.jpg", "images/14.jpg",
+     "images/15.jpg", "images/16.jpg", "images/17.jpg", "images/18.jpg", "images/19.jpg",
+     "images/20.jpg"];
     let currentIndex = 0;
 
     // Function to create a unique image element and add it to the gallery
@@ -71,4 +75,21 @@ document.addEventListener("DOMContentLoaded", function () {
             modalImage.src = imageFiles[currentIndex];
         }
     });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your existing code ...
+
+    const toggleButton = document.getElementById("toggleButton");
+    const body = document.body;
+    let isDarkMode = false;
+
+    // Function to toggle background color
+    function toggleBackgroundColor() {
+        isDarkMode = !isDarkMode;
+        body.style.backgroundColor = isDarkMode ? "black" : "white";
+    }
+
+    // Add a click event listener to the button
+    toggleButton.addEventListener("click", toggleBackgroundColor);
 });
